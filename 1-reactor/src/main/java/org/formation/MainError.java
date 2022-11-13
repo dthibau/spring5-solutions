9bfb6bfdc3bdb6e6f1ec32b74e671e6f927bc43f
David THIBAU <david.thibau@gmail.com>
2022-11-13 11:57:59 +0100

    1.3.2 OnError

--- 1-reactor/src/main/java/org/formation/MainError.java
+++ 1-reactor/src/main/java/org/formation/MainError.java
@@ -7,14 +7,22 @@ public class MainError {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MainError.methode1();
+		
+		MainError.methode2();
 	}
 
-
-	
 	public static void methode1() {
 
-		Flux.range(1, 10).concatWith(Flux.error(new RuntimeException()))
-		  .log()
-		  .subscribe();
+		Flux.range(1, 10).concatWith(Flux.error(new RuntimeException())).log().subscribe();
+	}
+
+	public static void methode2() {
+
+		Flux.range(1, 10)
+		.concatWith(Flux.error(new RuntimeException()))
+		.log().subscribe(
+				System.out::println,
+				e -> System.err.println(e)
+		);
 	}
 }
