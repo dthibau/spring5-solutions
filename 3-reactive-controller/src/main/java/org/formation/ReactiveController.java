78ad3e8b942472ce2e33fc45117b8e82a920aad6
David THIBAU <david.thibau@gmail.com>
2023-07-12 11:54:39 +0200

    Endpoint SSE and test with Webclient

--- 3-reactive-controller/src/main/java/org/formation/ReactiveController.java
+++ 3-reactive-controller/src/main/java/org/formation/ReactiveController.java
@@ -1,11 +1,14 @@
 package org.formation;
 
 import java.time.Duration;
+import java.time.LocalTime;
 
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -14,5 +17,16 @@ public class ReactiveController {
 	@GetMapping("/pause/{duration}")
 	public Mono<Void> pause(@PathVariable long duration) {
 		return Mono.delay(Duration.ofMillis(duration)).then();
+	}
+	
+	
+	@GetMapping("/stream-sse")
+	public Flux<ServerSentEvent<String>> streamEvents() {
+	    return Flux.interval(Duration.ofSeconds(1))
+	      .map(sequence -> ServerSentEvent.<String> builder()
+	        .id(String.valueOf(sequence))
+	          .event("periodic-event")
+	          .data("SSE - " + LocalTime.now().toString())
+	          .build());
 	}
 }
--- 3-reactive-controller/src/test/java/org/formation/ReactiveControllerApplicationTests.java
+++ 3-reactive-controller/src/test/java/org/formation/ReactiveControllerApplicationTests.java
@@ -1,13 +1,43 @@
 package org.formation;
 
+import java.time.LocalTime;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
 
 @SpringBootTest
+@Slf4j
 public class ReactiveControllerApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void testSSE() throws InterruptedException {
+		WebClient client = WebClient.create("http://localhost:8080");
+		
+		ParameterizedTypeReference<ServerSentEvent<String>> type
+	     = new ParameterizedTypeReference<ServerSentEvent<String>>() {};
+	     
+	     Flux<ServerSentEvent<String>> eventStream = client.get()
+	    	      .uri("/stream-sse")
+	    	      .retrieve()
+	    	      .bodyToFlux(type);
+	     
+	     eventStream.take(10).subscribe(
+	    		 content -> log.info("Time: {} - event: name[{}], id [{}], content[{}] ",
+	    			        LocalTime.now(), content.event(), content.id(), content.data()),
+	    			      error -> log.error("Error receiving SSE: {}", error),
+	    			      () -> log.info("Completed!!!"));
+
+	     Thread.sleep(15000);
+	}
 }
